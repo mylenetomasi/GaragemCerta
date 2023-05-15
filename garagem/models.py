7d91b25d05b90c76029abc59bdd1c5b7e247b306
mylenetomasi <mylenetomasi.jaque@gmail.com>
2023-05-14 22:16:37 -0300

    Criação do model Categoria

--- garagem/models.py
+++ garagem/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.nome.upper()
+    
+    
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+    
