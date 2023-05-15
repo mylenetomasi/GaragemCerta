4e9910d2fd5b7329ba2b44036d39d3e613afaf03
mylenetomasi <mylenetomasi.jaque@gmail.com>
2023-05-14 22:42:31 -0300

    Criação da model Veículo

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Acessorio, Categoria, Marca
+from .models import Acessorio, Categoria, Marca, Cor, Veiculo
 
 admin.site.register(Acessorio)
 admin.site.register(Categoria)
+admin.site.register(Cor)
 admin.site.register(Marca)
+admin.site.register(Veiculo)
