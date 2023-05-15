6d74875aa400ff2c1add339b6b9fc0de794fc422
mylenetomasi <mylenetomasi.jaque@gmail.com>
2023-05-14 22:51:31 -0300

    Criação API Categoria

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from garagem.views import MarcaViewSet
+from garagem.views import MarcaViewSet, CategoriaViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r"marcas", MarcaViewSet)
+router.register(r"categorias", CategoriaViewSet)
+
 urlpatterns = [path("admin/", admin.site.urls), path("", include(router.urls))]
