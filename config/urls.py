6048f0c5c81c3a4a151369e5ec5712f116751a1d
mylenetomasi <mylenetomasi.jaque@gmail.com>
2023-08-22 08:27:35 -0300

    criacao da tablea modelo e reorganizacao de pastas

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,18 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet, AcessorioViewSet, VeiculoViewSet 
+from garagem.views import (
+    MarcaViewSet,
+    CategoriaViewSet,
+    CorViewSet,
+    AcessorioViewSet,
+    VeiculoViewSet,
+    ModeloViewSet,
+)
 
 router = routers.DefaultRouter()
 router.register(r"marcas", MarcaViewSet)
+router.register(r"modelos", ModeloViewSet)
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"cores", CorViewSet)
 router.register(r"acessórios", AcessorioViewSet)
